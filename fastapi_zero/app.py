332b7539939ba6c3e9525b86dc1e4b57f5e6b18a
divirjo <vitor@divirjo.com.br>
2024-06-22 08:06:19 -0300

    Aula_02

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from fastapi_zero.schemas import Message
+
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
