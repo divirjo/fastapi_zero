866796ae71e8777b8a268250c68f5e7c74cf8bdd
divirjo <vitor@divirjo.com.br>
2024-06-23 09:02:24 -0300

    Aula 3: Implementando rotas CRUD

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,11 +1,75 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-from fastapi_zero.schemas import Message
+from fastapi import FastAPI, HTTPException
 
+from fastapi_zero.schemas import (
+    Message,
+    UserDB,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', response_model=Message)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+# Schema é checado ao enviar as informações UserPublic
+def create_user(user: UserSchema):
+    # Schema é checado ao receber as informações conforme UserSchema
+    user_with_id = UserDB(
+        id=len(database) + 1,
+        # converte todos os usuários objetos pydantic recebidos em
+        # um dicionário usuário
+        **user.model_dump(),
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(database):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found',
+        )
+
+    user_with_id = UserDB(
+        id=user_id,
+        **user.model_dump(),
+    )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,72 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Nome Usuário',
+            'email': 'usuario@fake.com',
+            'password': 'senha1',
+        },
+    )
+
+    # retornou o status code correto?
+    assert response.status_code == HTTPStatus.CREATED
+
+    # recebemos a resposta prevista?
+    assert response.json() == {
+        'id': 1,
+        'username': 'Nome Usuário',
+        'email': 'usuario@fake.com',
+    }
+
+
+def test_read_users(client):
+    # o pytest executa os testes em ordem alfabética,
+    # independente da posição no arquivo.
+    # um teste depender do anterior, não é uma boa prática
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Nome Usuário',
+                'email': 'usuario@fake.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'Nome atualizado',
+            'email': 'usuario@teste.com.br',
+            'password': 'nova_senha',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'Nome atualizado',
+        'email': 'usuario@teste.com.br',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
