96b72062aac58c56a05c123ac5b14a4846e94da6
root <root@DESKTOP-DLFLSPV>
2024-07-07 09:18:45 -0300

    Aula 4 - Banco de dados - parte 1 - até testes

---
+++ tests/test_db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session):
+
+    user = User(
+        username='test_user',
+        email='primeiro_user@divirjo.com.br',
+        password='secreta',
+    )
+
+    session.add(user)
+    session.commit()
+    result = session.scalar(
+        select(User).where(User.email == 'primeiro_user@divirjo.com.br')
+    )
+
+    assert result.username == 'test_user'
