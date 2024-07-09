e58fa65553f1c690116e4d85d511a5fe884329fe
divirjo <vitor@divirjo.com.br>
2024-07-09 09:28:46 -0300

    Início aula 5 - implementando o banco de dados nos endpoints

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,7 +1,10 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
 
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User
 from fastapi_zero.schemas import (
     Message,
     UserDB,
@@ -22,29 +25,48 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 # Schema é checado ao enviar as informações UserPublic
-def create_user(user: UserSchema):
-    # Schema é checado ao receber as informações conforme UserSchema
-    user_with_id = UserDB(
-        id=len(database) + 1,
-        # converte todos os usuários objetos pydantic recebidos em
-        # um dicionário usuário
-        **user.model_dump(),
+def create_user(user: UserSchema, session=Depends(get_session)):
+    db_user = session.scalar(
+                select(User).where(
+                    (User.username == user.username) |
+                    (User.email == user.email)
+                )
+            )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        if db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
+
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password=user.password,
     )
 
-    database.append(user_with_id)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)  # para retornar o id do usuário criado
 
-    return user_with_id
+    return db_user
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users():
+def read_users(session=Depends(get_session)):
     return {'users': database}
 
 
 @app.get(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def read_user(user_id: int):
+def read_user(user_id: int, session=Depends(get_session)):
     if user_id < 1 or user_id > len(database):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
@@ -58,7 +80,7 @@ def read_user(user_id: int):
 @app.put(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user_id: int, user: UserSchema):
+def update_user(user_id: int, user: UserSchema, session=Depends(get_session)):
     if user_id < 1 or user_id > len(database):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
@@ -74,7 +96,7 @@ def update_user(user_id: int, user: UserSchema):
 @app.delete(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
 )
-def delete_user(user_id: int):
+def delete_user(user_id: int, session=Depends(get_session)):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
