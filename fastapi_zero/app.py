88c0e8af65a20f64ac125f68767ddd80b0e30361
divirjo <vitor@divirjo.com.br>
2024-07-20 07:08:22 -0300

    Inicio Aula6: autenticação JWT e criptografar senhas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -12,6 +13,7 @@ from fastapi_zero.schemas import (
     UserPublic,
     UserSchema,
 )
+from fastapi_zero.security import get_password_hash
 
 app = FastAPI()
 
@@ -45,7 +47,7 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = User(
         username=user.username,
         email=user.email,
-        password=user.password,
+        password=get_password_hash(user.password),
     )
 
     session.add(db_user)
@@ -92,7 +94,7 @@ def update_user(user_id: int, user: UserSchema, session=Depends(get_session)):
 
     db_user.email = user.email
     db_user.username = user.username
-    db_user.password = user.password
+    db_user.password = get_password_hash(user.password)
 
     session.commit()
     session.refresh(db_user)
@@ -115,3 +117,10 @@ def delete_user(user_id: int, session=Depends(get_session)):
     session.commit()
 
     return {'message': 'User deleted'}
+
+@app.post('/token')
+def login_for_access_token(
+  form_data: OAuth2PasswordRequestForm = Depends(),
+  session: Session = Depends(get_session),
+):
+    ...
